f814572c77e0a6c328563ed7f016a9b7d460ae22
Shatteredbeam <shatteredbeam@gmail.com>
2012-03-07 17:48:11 -0800

    Add initial player handling

--- src/main/java/net/shatteredlands/arcanum/arcanum.java
+++ src/main/java/net/shatteredlands/arcanum/arcanum.java
@@ -1,8 +1,11 @@
 package net.shatteredlands.arcanum;
 
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permissible;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,12 +28,26 @@ public class arcanum extends JavaPlugin
 		 */
 	}
 	
-
+	@Override	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(label.equalsIgnoreCase("arcanum")) {
-			//Permission Check
+		if (command.getName().equalsIgnoreCase("arcanum")) {
+			
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Please run this command as a player.");
+				return true;
+			}
+		
+	
+			if (sender.hasPermission("arcanum.use")) {
+				//Call ArcanumGather Function here
+			return true;
+			}
+			
+			else {
+				sender.sendMessage(ChatColor.RED + "You do not have the required permission to use this command.");
+				return true;
+			}
 		}
-		//Do stuff on this command entry
-		return false;
-	}
+	return false;
+}
 }
